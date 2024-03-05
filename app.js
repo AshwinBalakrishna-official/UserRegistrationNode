706f3b551d543ce28a4c63c5ebe478f1aedda1ed
Ashwin <ashwinbkrish@gmail.com>
2024-03-05 12:20:31 +0530

    inital setup of userRegistration

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const msg = "BACKEND APP";
-console.log(msg);
-console.log("new commit");
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+
+app.listen("5003",()=>{console.log("Server is running at 5k port")});
+
+app.get("/",(req,res)=>{
+    res.send("<h1>HOME PAGE</h1>")
+})
